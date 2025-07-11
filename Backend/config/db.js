f55bdabf827da4b5074b725de3cf8442c4d2efd9
Naveen <naveenmcwork@gmail.com>
2025-07-11 15:56:11 +0530

    update mongo

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/college_portal', {
+    await mongoose.connect('mongodb://localhost:27017/college-portal', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
